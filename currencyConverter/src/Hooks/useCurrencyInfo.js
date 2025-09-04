c5fa4cbecddcfbf4eb6b466b1feadfc2a6765ae3
Visal Raza Zaidi <visalraza@gmail.com>
2025-09-04 23:21:10 +0500

    Refactor currency converter app: integrate InputBox component, enhance state management, and remove unused CSS imports

--- currencyConverter/src/Hooks/useCurrencyInfo.js
+++ currencyConverter/src/Hooks/useCurrencyInfo.js
@@ -1,12 +1,11 @@
 import {useEffect, useState} from 'react';
 
 function useCurrencyInfo (currency) {
-    const [data, setdata] = useState({}) //empty obj
+    const [data, setData] = useState({}) //empty obj
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res) => res.json)
-        .then((res) => setdata[currency]) //use [] for get excess of the obj
-        console.table(data)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency])) //use [] for get excess of the obj
     }, [currency]) 
     console.log(data)
     return data
