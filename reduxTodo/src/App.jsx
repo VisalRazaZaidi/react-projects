4c70e2eadff92461215db2c8e78878d124e42285
Visal Raza Zaidi <visalraza@gmail.com>
2025-09-15 16:47:25 +0500

    feat: integrate Tailwind CSS and implement AddTodo and Todos components

--- reduxTodo/src/App.jsx
+++ reduxTodo/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodos from './components/AddTodo'
+import Todos from "./components/Todos"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Redux ToolKit</h1>
+      <AddTodos />
+      <Todos />
     </>
   )
 }
