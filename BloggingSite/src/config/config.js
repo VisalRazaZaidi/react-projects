9e6aba0b6d8ed7d3bd44ce25dbc043822376f5d5
Visal Raza Zaidi <visalraza@gmail.com>
2025-09-18 21:51:28 +0500

    Created a new Mega project BlogSite

---
+++ BloggingSite/src/config/config.js
@@ -0,0 +1,10 @@
+const confs = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECTiD),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_PROJECTiD),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_PROJECTiD),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_PROJECTiD),
+}
+
+
+export default confs;
